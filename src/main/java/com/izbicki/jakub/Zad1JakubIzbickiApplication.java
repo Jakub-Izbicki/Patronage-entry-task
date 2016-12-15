cedd529e5ffcdaa48e991a6ef90655463a4f82ec
Jakub <jakub.izbicki1@gmail.com>
2016-12-16 00:49:14 +0100

    init commit

---
+++ src/main/java/com/izbicki/jakub/Zad1JakubIzbickiApplication.java
@@ -0,0 +1,12 @@
+package com.izbicki.jakub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zad1JakubIzbickiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Zad1JakubIzbickiApplication.class, args);
+	}
+}
