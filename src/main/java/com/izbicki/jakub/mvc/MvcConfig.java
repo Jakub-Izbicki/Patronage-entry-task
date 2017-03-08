7d83310b09f01283bb823eb8b30cb86f220e049d
Jakub <jakub.izbicki1@gmail.com>
2017-03-08 21:05:30 +0100

    changes swagger mapping

--- src/main/java/com/izbicki/jakub/mvc/MvcConfig.java
+++ src/main/java/com/izbicki/jakub/mvc/MvcConfig.java
@@ -9,7 +9,6 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
@@ -17,6 +16,8 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.time.LocalDate;
+
+import static springfox.documentation.builders.PathSelectors.regex;
 
 @EnableWebMvc
 @Configuration
@@ -39,7 +40,7 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
+                .paths(regex("/api/.+"))
                 .build()
                 .apiInfo(apiInfo())
                 .directModelSubstitute(LocalDate.class, String.class)
